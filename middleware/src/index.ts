24d1280f57d61d8198d89ebea5164d8c100ebf1d
irohitb <irohitbhatia@gmail.com>
2022-12-28 00:43:26 +0530

    feat: sql scripts

--- middleware/src/index.ts
+++ middleware/src/index.ts
@@ -5,6 +5,7 @@ import './services/firebase/init';
 
 // Start the server
 const port = Number(process.env.PORT || 3000);
+
 app.listen(port, () => {
   winston.log('info', 'Express server started on port: ' + port);
 });
